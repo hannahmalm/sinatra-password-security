120b163dcc533fc8ee806f5ebe07e640d3283135
Hannah Malm <hannahemalm616@gmail.com>
2021-08-23 20:55:58 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-	
-end
+    #this macro has_secure_password is being called like a normal ruby method
+    #it works in conjunction with a gem called bcrypt
+    #this will authenticate a user
+    #1. takes a string as an agrument 
+    #2. turns a string into a salted, hash version
+    #3. Compares salted, hash version with users stored salted hashed password
+    #4. If the versions match, authenticte will return the user if not it returns false
+	has_secure_password 
+end 
--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,7 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
-  end
+    create_table :users do |t|
+      t.string :username 
+      t.string :password_digest
+      #BCrypt will store a salted, hashed version of our users' passwords in our database in a column called password_digest. 
+    end
+  end 
   
   def down
+    drop_table :users
   end
 end
