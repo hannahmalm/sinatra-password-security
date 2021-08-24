f0d97ba4e0f396efb57415790a8eb457eec549b4
Hannah Malm <hannahemalm616@gmail.com>
2021-08-23 20:55:48 -0400

    commit tree 99fb86eee58beaf7c59c90b7ccb97349dbbaefbd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,16 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
+		user = User.new(:username => params[:username], :password => params[:password])
+
+		if user.save 
+			redirect "/login"
+		else 
+			redirect "/failure"
+		end 
+		#cant save the has_secure_password into the database unless our user filled out the password field
+		#calling user.save will return false if the user cannot be persisted
+		#login if the user is saved, or fail if the user is not saved
 	end
 
 	get "/login" do
@@ -25,7 +34,17 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
+		user = User.find_by(:username => params[:username])
+
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else 
+			redirect "/failure"
+		end 
+		#ensure that you have a User AND that the user is authenticated
+		#if the user is authenticated, set the session user id and redirect to success route
+		#check that if the users password matches with the value password_digest - users must have an account and know the passwrod
 	end
 
 	get "/success" do
